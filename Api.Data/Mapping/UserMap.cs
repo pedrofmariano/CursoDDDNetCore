8728e53a80b8fa14b9065d2dfd61f08be0196f1c
Pedro Henrique Fortunato Silva Mariano <pedro.mariano@localiza.com>
2021-01-08 10:02:47 -0300

    Add project files.

---
+++ Api.Data/Mapping/UserMap.cs
@@ -0,0 +1,19 @@
+﻿using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Data.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.ToTable("user");
+
+            builder.HasKey(p => p.Id);
+            builder.HasIndex(p => p.Email).IsUnique();
+            builder.Property(p => p.Email).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(60);
+        }
+    }
+}
---
+++ Api.Data/Migrations/20210107164229_First_Migration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Data.Migrations
+{
+    public partial class First_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateAt = table.Column<DateTime>(nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(maxLength: 60, nullable: false),
+                    Email = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_Email",
+                table: "user",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
